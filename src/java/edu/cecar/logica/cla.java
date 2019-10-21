1f032f88a81b7092917e2309d6b525c38a5bec61
1103220236 <1103220236@SJLBF02050526.admcecar.local>
2019-10-21 11:16:54 -0500

    primer commit

---
+++ src/java/edu/cecar/logica/cla.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cecar.logica;
+
+/**
+ *
+ * @author 1103220236
+ */
+public class cla {
+    
+}
