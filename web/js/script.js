7633c32998b774cb74a6cecc7d17c6e53413b940
testprove <jorge.acosta@cecar.edu.co>
2019-10-30 17:33:57 -0500

    se cambio el metodo de validacion de rubricas
    para validad el formulario de rubricas ahora se utiliza puramente
    javascript , se removieron las etiquetas required

--- web/js/script.js
+++ web/js/script.js
@@ -11,16 +11,33 @@ function validarFormularioRubrica(){
     cantidad_aspectos = document.getElementById("cantidad_aspectos").value;
     cantidad_sub_aspectos = document.getElementById("cantidad_sub_aspectos").value;
     cantidad_desemp = document.getElementById("cantidad_desemp").value;
+    document.getElementById("nombres").style.borderColor = "#FFFFFF";
+    document.getElementById("descripcion").style.borderColor = "#FFFFFF";
+    document.getElementById("cantidad_aspectos").style.borderColor = "#FFFFFF";
+    document.getElementById("cantidad_sub_aspectos").style.borderColor = "#FFFFFF";
+    document.getElementById("cantidad_desemp").style.borderColor = "#FFFFFF";
     
     if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)){
+        document.getElementById("nombres").style.borderColor = "#F52C01";
         return false;
     }else if(descripcion == null || descripcion.length == 0 || /^\s+$/.test(descripcion)){
+        document.getElementById("descripcion").style.borderColor = "#F52C01";
         return false;
-    }else if(cantidad_aspectos == null || cantidad_aspectos.length == 0 || /^\s+$/.test(cantidad_aspectos) || isNaN(cantidad_aspectos)){
+    }else if(cantidad_aspectos == null || cantidad_aspectos.length == 0 || /^\s+$/.test(cantidad_aspectos) || isNaN(cantidad_aspectos) || cantidad_aspectos<=0){
+        document.getElementById("cantidad_aspectos").style.borderColor = "#F52C01";
+        if (cantidad_aspectos<=0){
+            alert("el valor de numero de aspectos no puede ser cero ni negativo")
+        }
+        
         return false;
     }else if (cantidad_sub_aspectos == null || cantidad_sub_aspectos.length == 0 || /^\s+$/.test(cantidad_sub_aspectos) || isNaN(cantidad_sub_aspectos)){
+        document.getElementById("cantidad_sub_aspectos").style.borderColor = "#F52C01";
         return false;
-    }else if(cantidad_desemp == null || cantidad_desemp.length == 0 || /^\s+$/.test(cantidad_desemp) || isNaN(cantidad_desemp)){
+    }else if(cantidad_desemp == null || cantidad_desemp.length == 0 || /^\s+$/.test(cantidad_desemp) || isNaN(cantidad_desemp) || cantidad_desemp<=0){
+        document.getElementById("cantidad_sub_aspectos").style.borderColor = "#F52C01";
+        if (cantidad_desemp<=0){
+            alert("el valor de cantidad de desenpeño no puede ser cero ni negativo")
+        }
         return false;
     }else{
         return true;
