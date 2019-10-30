fb4a9d823fe460845a84cca1293794d3602cba68
testprove <jorge.acosta@cecar.edu.co>
2019-10-30 16:56:34 -0500

    se agrego la funcion para validar el formulario rubricas
    re agrego la funcion de validar y se agrgo la etiqueta requied en los
    campos del formulario

--- web/js/script.js
+++ web/js/script.js
@@ -5,3 +5,25 @@
  */
 
 
+function validarFormularioRubrica(){
+    nombre = document.getElementById("nombres").value;
+    descripcion = document.getElementById("descripcion").value;
+    cantidad_aspectos = document.getElementById("cantidad_aspectos").value;
+    cantidad_sub_aspectos = document.getElementById("cantidad_sub_aspectos").value;
+    cantidad_desemp = document.getElementById("cantidad_desemp").value;
+    
+    if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)){
+        return false;
+    }else if(descripcion == null || descripcion.length == 0 || /^\s+$/.test(descripcion)){
+        return false;
+    }else if(cantidad_aspectos == null || cantidad_aspectos.length == 0 || /^\s+$/.test(cantidad_aspectos) || isNaN(cantidad_aspectos)){
+        return false;
+    }else if (cantidad_sub_aspectos == null || cantidad_sub_aspectos.length == 0 || /^\s+$/.test(cantidad_sub_aspectos) || isNaN(cantidad_sub_aspectos)){
+        return false;
+    }else if(cantidad_desemp == null || cantidad_desemp.length == 0 || /^\s+$/.test(cantidad_desemp) || isNaN(cantidad_desemp)){
+        return false;
+    }else{
+        return true;
+    }
+    
+}
